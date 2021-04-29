9303c7dd41d0c16a9a226091eccd8ef824198eed
Stephanie Ho <stephanie.e.ho@gmail.com>
2021-04-29 11:57:23 -0500

    updating pkg to 1.14 and perm set names

--- orgInit.sh
+++ orgInit.sh
@@ -4,31 +4,30 @@
 # to actually execute: bash orgInit.sh
 #
 # get alias from user 
-echo "this is a 1.11 sustainability scratch. name your org:"
+echo "this is a 1.14 sustainability scratch. name your org:"
 read sname
 
 echo "your org alias is $sname"
 echo creating your scratch org... 
 sfdx force:org:create -f config/project-scratch-def.json -s -a $sname
 
-# install 1.12.0
-sfdx force:package:install -p 04t3k000000iy8qAAA -w 20 
-
 # PSL/Perms 
 # using shane's plugins https://github.com/mshanemc/shane-sfdx-plugins
+# i am pretty sure the xDOs we'll need to assign the PSL to the running user first...
+# sfdx shane:user:psl -l User -g User -n sustain_app_SustainabilityCloudPsl
+# sfdx shane:user:psl -l User -g User -n InsightsInboxAdminAnalyticsPsl
 
-sfdx shane:user:psl -l User -g User -n sustain_app_SustainabilityCloudPsl
-sfdx shane:user:psl -l User -g User -n InsightsInboxAdminAnalyticsPsl
+# install 1.14.0
+sfdx force:package:install -p 04t3k0000027CZ1AAM -w 20 
 
-sfdx force:user:permset:assign -n SustainabilityAnalytics
-sfdx force:user:permset:assign -n SustainabilityAppAuditor
-sfdx force:user:permset:assign -n SustainabilityAppManager
 sfdx force:user:permset:assign -n SustainabilityCloud
+sfdx force:user:permset:assign -n SustainabilityAppManager
+sfdx force:user:permset:assign -n SustainabilityAnalytics
+sfdx force:user:permset:assign -n EventMonitoringWaveAdmin 
 
 sfdx shane:user:permset:assign -l User -g Integration -n SustainabilityAnalytics
 
 #load data 
-sfdx automig:load -d demo-data/ --concise --mappingobjects RecordType:DeveloperName,sustain_app__EmissionFactorElectricity__c:Name,sustain_app__EmissionFactorOther__c:Name,sustain_app__EmissionFactorScope3__c:Name
 
 #create EA apps
 #sfdx analytics:app:create --templatename Sustainability
@@ -40,4 +39,4 @@ sfdx automig:load -d demo-data/ --concise --mappingobjects RecordType:DeveloperN
 #sfdx force:source:push
 
 # open sesame
-sfdx force:org:open 
+# sfdx force:org:open 
